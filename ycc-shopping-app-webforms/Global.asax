a82fb12bc03636eb8f1951577bcf24d35cdb908a
tuza john <johntuza94@gmail.com>
2018-03-30 16:41:23 +0300

    new classes

---
+++ ycc-shopping-app-webforms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ycc_shopping_app_webforms.Global" Language="C#" %>
---
+++ ycc-shopping-app-webforms/SessionVerification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Customs;
+using ShoppingLibrary;
+
+
+namespace ycc_shopping_app_webforms
+{
+    public class SessionVerification:System.Web.UI.Page
+    {
+        RegExpression reg = new RegExpression();
+        Encryption enc = new Encryption() { Key = "" };
+        
+        public SessionVerification()
+        {
+            var (check, result) = reg.IsPassword("john");
+            if (check)
+            {
+                Session["password"] = enc.GetMD5(enc.StrongEncrypt("john"));
+            }
+            else { Session["message"] = result; }
+        }
+    }
+}
